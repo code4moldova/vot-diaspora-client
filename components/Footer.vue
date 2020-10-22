6cce53dd5ee9afee3992220f12424de95014819e
Tatiana <43054051+tacoelho@users.noreply.github.com>
2020-10-22 16:46:51 -0300

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -164,7 +164,7 @@
               <img
               alt="Code4mo"
               src="../assets/code4moldova.png"
-              class="mt-3 mb-2" /</nuxt-link
+              class="mt-3 mb-2" /></nuxt-link
           ><br />
           <span>&copy; 2020 Code for Romania.</span>  
           <span>&copy; 2020 Code for Moldova.</span>
