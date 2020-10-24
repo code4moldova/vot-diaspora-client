5e5e0f7346ebdf22840f331b124b2a9d4271c135
Andrew Luca <thendrluca@gmail.com>
2020-10-24 08:52:50 +0300

    fix: adapt backend URL to local development instance
    Closes #14

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   env: {
     NUXT_ENV_API_URL:
-      process.env.NUXT_ENV_API_URL || 'https://api.votdiaspora.ro/api',
+      process.env.NUXT_ENV_API_URL || 'http://185.181.229.182:4000/api',
   },
   /*
    ** Nuxt target
