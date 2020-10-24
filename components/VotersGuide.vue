8202c9e094073afd894a48528359a6b7e727c8b7
Sergiu <synthezys@gmail.com>
2020-10-24 13:25:53 +0300

    fix: adapt the landing page text

--- components/VotersGuide.vue
+++ components/VotersGuide.vue
@@ -1,89 +1,10 @@
 <template>
-  <div>
-    <h2 class="text-center h4">
-      {{ $t('guide_title') }}
-    </h2>
-    <p>
-      La alegerile parlamentare își exercită dreptul de vot în afara țării doar
-      cetățenii români cu domiciliul sau reședința în străinătate. Află ce ai de
-      făcut pentru a putea vota în țara în care ai domiciliu sau reședința la
-      alegerile parlamentare din 6 decembrie 2020.
-    </p>
-    <div
-      class="d-flex flex-column flex-sm-row justify-content-center align-items-center align-items-sm-stretch mt-3"
-    >
-      <div
-        v-for="option in initial.options"
-        :key="option"
-        class="card"
-        :class="{
-          'choice-card text-center my-2 mx-0 mx-sm-2 font-weight-bolder':
-            decisionTree[option].options,
-        }"
-        @click="goToNextChoice(option)"
-      >
-        <div class="card-body d-flex align-items-center justify-content-center">
-          <p class="my-0" v-html="$t(decisionTree[option].text)"></p>
-        </div>
-      </div>
-    </div>
-    <div
-      v-if="previousChoice.length"
-      class="d-flex justify-content-between my-4"
-    >
-      <button class="btn btn-light" @click="resetTest()">
-        {{ $t('back_to_start_button') }}
-      </button>
-      <button class="btn btn-light" @click="goToPreviousChoice()">
-        {{ $t('back_button') }}
-      </button>
-    </div>
-  </div>
+  <div v-html="$t('votersGuide.textHeader')" />
 </template>
 
 <script>
 export default {
-  data: () => ({
-    previousChoice: [],
-    initial: { options: [0, 1] },
-    decisionTree: {
-      0: {
-        id: '0',
-        text: 'votersGuide.label0',
-        options: ['10'],
-      },
-      1: {
-        id: '1',
-        text: 'votersGuide.label1',
-        options: ['11'],
-      },
-      10: {
-        id: '10',
-        text: 'votersGuide.label10',
-      },
-      11: {
-        id: '11',
-        text: 'votersGuide.label11',
-      },
-    },
-  }),
-  methods: {
-    goToPreviousChoice() {
-      const previousOption = this.previousChoice.pop()
-      this.initial.options = previousOption
-    },
-    goToNextChoice(option) {
-      if (!this.decisionTree[option].options) {
-        return
-      }
-      this.previousChoice.push(this.initial.options)
-      this.initial.options = this.decisionTree[option].options
-    },
-    resetTest() {
-      this.previousChoice = []
-      this.initial.options = ['0', '1']
-    },
-  },
+  name: 'VotersGuide',
 }
 </script>
 
