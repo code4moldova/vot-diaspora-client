948e2748dacd1e8902096b803d9cf67f6dec7fc1
Mosnoi Ion <johnsmithm@users.noreply.github.com>
2020-10-26 09:58:20 +0200

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,7 @@ export default {
       { property: 'og:title', content: 'Vot Diaspora Republica Moldova' },
       {
         property: 'og:image',
-        content: 'https://votdiaspora.md/logov4.png',
+        content: 'logov4.png',
       },
       {
         name: 'theme-color',
